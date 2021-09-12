e8977afc082180ec65ca189316712964863f035d
RJ Atwal <atwal.rj@gmail.com>
2021-09-12 00:41:03 -0700

    Cleaned up the Static Search Tree interface

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,9 @@ mod search_tree;
 
 use search_tree::SearchTree;
 
-
-/*
-             0
-         0       4
-       0   2   4   6
-      0 1 2 3 4 5 6 7
- */
 fn main() {
-    let array = [0,0,4,  0,0,1,  2,2,3,  4,4,5,  6,6,7];
-    let search_tree = SearchTree::new(&array, 3);
-    search_tree.search(6);
+    let array: Vec<i32> = (0..8).collect();
+    if let Ok(search_tree) = SearchTree::new(array.into_iter(), 8) {
+        search_tree.search(6);
+    }
 }
--- src/search_tree/mod.rs
+++ src/search_tree/mod.rs
@@ -4,18 +4,46 @@ mod util;
 
 use search::SearchTreeIndex;
 use search::search_for_lower_bound;
+use crate::search_tree::create::layout;
 
-pub struct SearchTree<'a> {
-    array: &'a[i32],
+pub struct SearchTree {
+    array: Box<[i32]>,
     height: u16
 }
 
-impl SearchTree<'_> {
-  pub fn new(array: &[i32], height: u16) -> SearchTree {
-      SearchTree { array, height }
-  }
+impl SearchTree {
+    pub fn search(&self, element: i32) -> SearchTreeIndex {
+        search_for_lower_bound(element, self.height, &self.array)
+    }
 
-  pub fn search(&self, element: i32) -> SearchTreeIndex {
-      search_for_lower_bound(element, self.height, self.array)
-  }
+    pub fn new<'a>(generator: impl Iterator<Item=i32>, count: usize) -> Result<SearchTree, ()>{
+        assert_eq!(count.count_ones(), 1,
+                   "Search Tree must be a full binary tree. Number of leaves: {}", count);
+
+        let height = count.trailing_zeros()  as u16 + 1;
+        let size = count*2-1;
+
+        let mut reserved_space = vec![0; size];
+        layout(&mut reserved_space, Box::new(generator), height).map(
+            move |_i| SearchTree { array: reserved_space.into_boxed_slice(), height}
+        )
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::search_tree::search::{SearchTreeIndex};
+    use crate::search_tree::SearchTree;
+
+    #[test]
+    fn create_and_search() {
+        let leaves : Vec<i32> = (0..32).chain((40..=360).step_by(10)).collect();
+        let search_tree = SearchTree::new(leaves.into_iter(), 64).unwrap();
+
+        assert_eq!(search_tree.search(39),
+            SearchTreeIndex::Leaf{index: 70, leaf_number: 31},
+            "Searching for element in tree's span, but not in tree. Expecting lower bound"
+        );
+    }
 }
